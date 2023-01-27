bc378d6c1d21fde20f2bf94b5b7c83fb0c6b107e
Bryan Rodriguez <brod26@gmail.com>
2023-01-26 23:26:17 -0800

    fixed breaking MUI errors, implemented scaffolding for SignIn and SignUp components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+
+const theme = createTheme();
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignIn/> 
     </div>
+    </ThemeProvider>
   );
 }
 
