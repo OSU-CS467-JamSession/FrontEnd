471aeb4ddc749b8ed8b5c60acc47e7ec1f428d41
Dan P <me@dipanp.com>
2023-03-07 23:45:25 -0600

    Changed signin to use new login API endpoint

---
+++ src/components/services/authenticateUser.jsx
@@ -0,0 +1,50 @@
+export async function authenticateUserQ(credentialsObject){
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*", 'mode':'no-cors'},
+    };
+
+    try{
+        function promisedParseJSON(json) {
+            return new Promise((resolve, reject) => {
+                try {
+                    resolve(JSON.parse(json))
+                } catch (e) {
+                    reject(e)
+                }
+            })
+        }
+        const email = credentialsObject.email
+        const password = credentialsObject.password
+
+        const body = {
+            "email": email,
+            "password": password
+        }
+
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            'mode': 'no-cors',
+            // 'Authorization': `Bearer ${token}`,
+        }
+        console.log(body)
+
+        const response = await fetch('https://jamsession-cs467-w2023.uw.r.appspot.com/login', {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body),
+        })
+
+        const parsedValue = await response.json();
+
+        const responseObject = {status: response.status, content: parsedValue};
+        console.log(responseObject);
+        return responseObject;
+    }
+    catch(error){
+        console.log(error)
+    }
+}
